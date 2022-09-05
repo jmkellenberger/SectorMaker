d1cbb1703156811f1062780d5809affef7100cc9
Jack Kellenberger <jackson@kellenberger.io>
2022-09-05 00:06:10 -0400

    Set base page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "| Aamidal"
+    @base_title = "Aamidal, Sci-fi Adventures in the Three Sisters"
   end
 
   test "should get root" do
@@ -13,18 +13,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home #{@base_title}"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About #{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
